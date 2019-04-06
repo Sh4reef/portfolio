e6429a1deb4480ef6dc23a9f5949eaa09aab6963
Sh4reef <shareef.banjar@gmail.com>
2019-04-07 01:45:44 +0800

    Updates

---
+++ precache-manifest.0a4166ff2129800e91db140cb18fb088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48d8cffd6855b9adcd2a",
+    "url": "/static/css/main.25481d7d.chunk.css"
+  },
+  {
+    "revision": "48d8cffd6855b9adcd2a",
+    "url": "/static/js/main.4e37aa3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d37813baefa601c1ff76",
+    "url": "/static/js/2.225f953a.chunk.js"
+  },
+  {
+    "revision": "998c3ba3793c42632a9ca696abcff50e",
+    "url": "/index.html"
+  }
+];
