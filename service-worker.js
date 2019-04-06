ca8e1e84f33a048174e846038537a4a4e4e97637
Sh4reef <shareef.banjar@gmail.com>
2019-04-07 02:17:10 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.090ff7ac184f8ac7a4b711753b047fdb.js"
+  "/portfolio/precache-manifest.dc44a1abcc97eee27e6d36e49ea2041c.js"
 );
 
 workbox.clientsClaim();
